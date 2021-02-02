a3892aeae420c3b823f8f7b8b08d7540db674a56
出島璃空 <>
2021-02-02 11:22:51 +0900

    エラー！！！

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,18 @@ void main() {
 
 class MyApp extends StatelessWidget {
   MaterialColor customSwatch = const MaterialColor(
-    0xfffff7cb,
+    0xffFFF7CB,
     const <int, Color>{
-      50: const Color(0xfffffef9),
-      100: const Color(0xfffffdef),
-      200: const Color(0xfffffbe5),
-      300: const Color(0xfffff9db),
-      400: const Color(0xfffff8d3),
-      500: const Color(0xfffff7cb),
-      600: const Color(0xfffff6c6),
-      700: const Color(0xfffff5be),
-      800: const Color(0xfffff3b8),
-      900: const Color(0xfffff1ac),
+      50: Color(0xFFFFFEF9),
+      100: Color(0xFFFFFDEF),
+      200: Color(0xFFFFFBE5),
+      300: Color(0xFFFFF9DB),
+      400: Color(0xFFFFF8D3),
+      500: Color(0xffFFF7CB),
+      600: Color(0xFFFFF6C6),
+      700: Color(0xFFFFF5BE),
+      800: Color(0xFFFFF3B8),
+      900: Color(0xFFFFF1AC),
     },
   );
 
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:labbit/pages/create_account.dart';
+import 'package:labbit/models/user.dart';
 
+final GoogleSignIn googleSignIn = GoogleSignIn();
+FirebaseFirestore firestore = FirebaseFirestore.instance;
+// final StorageReference storageRef = FirebaseStorage.instance.ref();
+final CollectionReference usersRef =
+    FirebaseFirestore.instance.collection('users');
+final CollectionReference postsRef =
+    FirebaseFirestore.instance.collection('posts');
+final DateTime timestamp = DateTime.now();
+User currentUser;
 //グーグルサイン認証、firebaseでtodoの更新などを行う
 
 class MyHomePage extends StatefulWidget {
+  //statefulWidgetは引数を受け取るための場所
   MyHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
@@ -12,30 +27,113 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  bool isAuth = false;
+  PageController pageController;
+  int pageIndex = 0;
+  // User currentUser;
+
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
+    pageController = PageController();
+    googleSignIn.onCurrentUserChanged.listen((account) {
+      handleSignIn(account);
+    }, onError: (err) {
+      print("Error signing in: $err");
+    });
+
+    googleSignIn.signInSilently(suppressErrors: false).then((account) {
+      handleSignIn(account);
+    });
+  }
+
+  handleSignIn(GoogleSignInAccount account) {
+    if (account != null) {
+      createUserInFirestore();
+      print("User signed in!: $account");
+      setState(() {
+        isAuth = true;
+      });
+    } else {
+      setState(() {
+        isAuth = false;
+      });
+    }
+  }
+
+  createUserInFirestore() async {
+    final GoogleSignInAccount user = googleSignIn.currentUser;
+    print(user);
+    final DocumentSnapshot doc = await FirebaseFirestore.instance
+        .collection("usersRef")
+        .doc(user.id)
+        .get();
+
+    if (!doc.exists) {
+      final username = await Navigator.push(
+          context, MaterialPageRoute(builder: (context) => CreateAccount()));
+      usersRef.doc(user.id).set({
+        "id": user.id,
+        "username": username,
+        // "photoUrl": user.photoUrl,
+        "email": user.email,
+        // "displayName": user.displayName,
+        // "bio": "",
+        "timestamp": timestamp,
+      });
+    }
+    currentUser = User.fromDocument(doc);
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  login() {
+    googleSignIn.signIn();
+  }
+
+  logout() {
+    googleSignIn.signOut();
+  }
+
+  onPageChanged(int pageIndex) {
+    setState(() {
+      this.pageIndex = pageIndex;
+    });
+  }
+
+  onTap(int pageIndex) {
+    pageController.animateToPage(pageIndex,
+        duration: Duration(milliseconds: 300), curve: Curves.easeInOut);
+  }
+
+  buildAuthScreen() {
     return Scaffold(
       appBar: AppBar(
+          toolbarHeight: 70,
           title: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Image.asset(
-            "assets/images/appbar_icon/hamburger_menu.png",
-            fit: BoxFit.cover,
-            height: 32,
-          ),
-          Image.asset(
-            "assets/images/appbar_icon/labbit_logo.png",
-            fit: BoxFit.cover,
-            height: 32,
-          ),
-          Image.asset(
-            "assets/images/appbar_icon/user_icon.png",
-            fit: BoxFit.cover,
-            height: 32,
-          ),
-        ],
-      )),
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                "assets/images/appbar_icon/hamburger_menu.png",
+                fit: BoxFit.cover,
+                height: 32,
+              ),
+              Image.asset(
+                "assets/images/appbar_icon/labbit_logo.png",
+                fit: BoxFit.cover,
+                height: 32,
+              ),
+              Image.asset(
+                "assets/images/appbar_icon/user_icon.png",
+                fit: BoxFit.cover,
+                height: 32,
+              ),
+            ],
+          )),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -46,11 +144,59 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+      backgroundColor: Color(0xffFFFEEB),
+
       floatingActionButton: FloatingActionButton(
         onPressed: () => print("pressed"),
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Scaffold buildUnAuthScreen() {
+    return Scaffold(
+      body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+              Theme.of(context).primaryColor,
+              Theme.of(context).accentColor
+            ],
+            begin: Alignment.topRight,
+            end: Alignment.topLeft,
+          )),
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "labbit",
+                style: TextStyle(
+                    // fontFamily: "Signatra",
+                    fontSize: 40.0,
+                    color: Colors.white),
+              ),
+              GestureDetector(
+                onTap: () => login(),
+                child: Container(
+                  width: 260.0,
+                  height: 60.0,
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage(
+                              "assets/images/google_signin_button.png"),
+                          fit: BoxFit.cover)),
+                ),
+              )
+            ],
+          )),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isAuth ? buildAuthScreen() : buildUnAuthScreen();
   }
 }
